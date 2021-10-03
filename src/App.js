3710844aba516109e6abf3283d3d7b8754cd6d2d
aktarulrahul <contact.aktarul@gmail.com>
2021-10-04 01:13:28 +0600

    route setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Services from './components/Services/Services';
+import Contact from './components/Contact/Contact';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from App.js</h1>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/CourseCard/CourseCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CourseCard.css';
+
+const CourseCard = () => {
+  return (
+    <div>
+      <h1>CourseCard</h1>
+    </div>
+  );
+};
+
+export default CourseCard;
